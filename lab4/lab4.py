f143949a126b1e8ed13d1b6fc1c8936b57f40be5
tutaf <andrei.cernisov@isa.utm.md>
2024-03-31 19:33:53 +0300

    step by step generation for pattern 1

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,27 +1,71 @@
 import re, random
+import string
 
-regex1 = 'O(P|Q|R)+2(3|4)'
+REPETITION_LIMIT = 5
 
 
-def generate_example(pattern: str):
-    attempt = 0
-    non_regex_symbols = pattern
-    regex_symbols = ['[', ']', '(', ')', '{', '}', '*', '+', '.', '^', '$', '\\', '|', '?']
-    for symbol in regex_symbols:
-        non_regex_symbols = non_regex_symbols.replace(symbol, '')
-    non_regex_symbols = list(set(list(non_regex_symbols)))
-    # print(non_regex_symbols)
+class Step:
+    def __init__(self):
+        self.symbols = []  # '?' - any symbol
+        self.repetitions = 0  # '*' - zero or more; '+' - once or more
 
-    while True:
+    def get_matching_string_and_description(self):
         result = ""
-        for i in range(random.randint(2, 10)):
-            result += random.choice(non_regex_symbols)
-        result_string = "".join(result)
-        if re.fullmatch(pattern, result_string):
-            return result_string
+        progress = []
+        description = ""
+        exact_repetitions = 0
+        if self.repetitions == '*':
+            description = "zero or more times"
+            exact_repetitions = random.randint(0, REPETITION_LIMIT)
+        elif self.repetitions == '+':
+            description = "one or more times"
+            exact_repetitions = random.randint(1, REPETITION_LIMIT)
         else:
-            attempt += 1
+            description = f"exactly {self.repetitions} times"
+            exact_repetitions = self.repetitions
+
+        description = f"Picking random element from {self.symbols} " + description
+
+        for i in range(exact_repetitions):
+            choice = random.choice(self.symbols)
+            if choice == '?':
+                choice = random.choice(string.ascii_lowercase)
+            result += choice
+            progress.append(result)
+
+        progress_string = " -> ".join(progress)
+
+        return result, description, progress_string
 
 
-r = generate_example(regex1)
-print(r)
+def generate_steps(regex):
+    for symbols, repetitions in regex:
+        step = Step()
+        step.symbols = symbols
+        step.repetitions = repetitions
+        yield step
+
+
+regex1_pattern = 'O(P|Q|R)+2(3|4)'
+regex1 = [
+    (['O'], 1),
+    (['P', 'Q', 'R'], '+'),
+    (['2'], 1),
+    (['3', '4'], 1)
+]
+
+steps_generator = generate_steps(regex1)
+steps = list(steps_generator)
+
+regex_result = ""
+for i in range(len(steps)):
+    step = steps[i]
+    result, description, progress_string = step.get_matching_string_and_description()
+    print(f"Step {i}: {description}")
+    print(f"{progress_string}")
+    regex_result += result
+
+print(f"\nResult: {regex_result}")
+print(f"Is a valid string: {re.fullmatch(regex1_pattern, regex_result) is not None}")
+
+
