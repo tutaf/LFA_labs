d6f82ad5745d1304d47bf57521b63750b5819791
tutaf <the.tutaf@yandex.kz>
2024-04-28 10:05:11 +0300

    paragraph parsing

--- 6_ParserASTBuild/main.py
+++ 6_ParserASTBuild/main.py
@@ -1,4 +1,5 @@
 import re
+from typing import List
 
 
 class TokenKind:
@@ -33,6 +34,7 @@ TOKEN_PATTERNS = [
     (TokenKind.WHITESPACE, re.compile(r'^\s+')),
     (TokenKind.TEXT, re.compile(r'^[^\s\[\]#!\*_\(\)-]+')),
 ]
+
 
 class Token:
     def __init__(self, kind, value, position):
@@ -69,14 +71,62 @@ def lexer(input_text):
             position += 1
     return tokens
 
+
+class NodeType:
+    ROOT = 'ROOT'
+    PARAGRAPH = 'PARAGRAPH'
+    BOLD = 'BOLD'
+    ITALIC = 'ITALIC'
+    IMAGE = 'IMAGE'
+    LINK = 'LINK'
+    TEXT = 'TEXT'
+
+
+class Node:
+    def __init__(self, node_type, node_value, input_tokens: List[Token]):
+        self.node_type = node_type
+        self.node_value = node_value
+        self.children = []
+        self.tokens = input_tokens
+
+        if node_type == NodeType.ROOT:
+            self.__process_paragraphs__()
+
+    def __process_paragraphs__(self):
+        paragraph_contents = []
+        for token in self.tokens:
+            if token.kind == TokenKind.NEWLINE and len(paragraph_contents) > 0:
+                self.children.append(Node(NodeType.PARAGRAPH, None, paragraph_contents))
+                paragraph_contents = []
+
+            if token.kind != TokenKind.NEWLINE:
+                paragraph_contents.append(token)
+
+
+    # def __parse_paragraph_contents__(self):
+
+
+    # def __str__(self):
+    #
+    #     return f"{self.node_type}: {self.node_value}, {self.children} ||| {self.tokens}"
+
+
+
 markdown_text = """
-# Heading
 [Link](http://example.com)
 ![Image](http://example.com/img.png)
 Text   here
 *italic* **bold** ***bold AND italic***
+*italic text* **[some link](http://example.com) some other text** ***bold AND italic***
 """
 
-tokens = lexer(markdown_text)
-for token in tokens:
-    print(token)
+
+example_tokens = lexer(markdown_text)
+for example_token in example_tokens:
+    print(example_token)
+
+
+print("\n===========================\n")
+node = Node(NodeType.ROOT, None, example_tokens)
+print(node)
+
